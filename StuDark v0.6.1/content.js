874284b037cecbb863f0b45e2ad818dff24f0ae8
Goldenbeasty <73758693+Goldenbeasty@users.noreply.github.com>
2020-10-31 21:52:27 +0200

    Add files via upload

---
+++ StuDark v0.6.1/content.js
@@ -0,0 +1,55 @@
+
+	let terasubheader = document.getElementsByTagName("p");
+	for(elt of terasubheader)
+	{
+		elt.style['background-color'] = 'rgba(40,40,40,0.98)';
+		elt.style['color'] = 'white';
+	}
+
+	let generic = document.getElementsByTagName("span");
+	for(elt of generic)
+	{
+		elt.style['background-color'] = 'rgba(40,40,40,0.98)';
+		elt.style['color'] = 'white';
+	}
+	let lbold = document.getElementsByTagName("h2");
+	for(elt of lbold)
+	{
+		elt.style['background-color'] = 'rgba(40,40,40,0.98)';
+		elt.style['color'] = 'white';
+	}
+	let mainbackground = document.getElementsByTagName("div");
+	for(elt of mainbackground)
+	{
+		elt.style['background-color'] = 'rgba(40,40,40,0.98)';
+		elt.style['color'] = 'white';
+	}
+	let heading = document.getElementsByTagName("h3");
+	for(elt of heading)
+	{
+		elt.style['background-color'] = 'rgba(40,40,40,0.98)';
+		elt.style['color'] = 'white';
+	}
+	let parrentname = document.getElementsByTagName("i");
+	for(elt of parrentname)
+	{
+		elt.style['color'] = 'white';
+	}
+	let hyperlinks = document.getElementsByTagName("a");
+	for(elt of hyperlinks)
+	{
+		elt.style['color'] = 'rgb(87, 170, 242)';
+		elt.style['background-color'] = 'rgba(40,40,40,0.98)';
+	}
+	let kodutoo = document.getElementsByTagName("strong");
+	for(elt of kodutoo)
+	{
+		elt.style['color'] = '#57aaff';
+	}
+	let suhtlus = document.getElementsByTagName("h1");
+	for(elt of suhtlus)
+	{
+		elt.style['background-color'] = 'rgba(40,40,40,0.98)';
+		elt.style['color'] = 'white';
+	}
+	// Oliver 2020/21 TTG 10.TE
